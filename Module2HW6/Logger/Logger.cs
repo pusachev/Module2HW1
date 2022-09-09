3606eff528ab392dd91e5d91c9a528ee862cd5bf
Pavel Usachev <pausachev@gmail.com>
2022-09-09 23:37:51 +0300

    added basic logic for logger

--- Module2HW6/Logger/Logger.cs
+++ Module2HW6/Logger/Logger.cs
@@ -4,9 +4,23 @@ namespace Module2HW6.Logger
 {
     public sealed class Logger
     {
-        private Logger() { }
+        private enum Level : byte
+        {
+            Info = 1,
+            Warn = 2,
+            Error = 3,
+            Debug = 4
+        }
+
+        private readonly string _filename = "system.log";
+        private StreamWriter _writer;
 
         private static Logger? _instance;
+
+        private Logger()
+        {
+            _writer = new StreamWriter(_filename);
+        }
 
         public static Logger GetInstance()
         {
@@ -16,6 +30,66 @@ namespace Module2HW6.Logger
             }
 
             return _instance;
+        }
+
+        public void Info(string Message)
+        {
+            Log(Level.Info, Message);
+        }
+
+        public void Warning(string Message)
+        {
+            Log(Level.Warn, Message);
+        }
+
+        public void Error(string Message)
+        {
+            Log(Level.Error, Message);
+        }
+
+        public void Debug(string Message)
+        {
+            Log(Level.Debug, Message);
+        }
+
+        private void Log(Level Level, string Message, Object? Context = null)
+        {
+            string logMessage;
+
+            switch (Level)
+            {
+                case Level.Info:
+                {
+                    logMessage = "INFO: ";
+                    break;
+                }
+                case Level.Warn:
+                {
+                    logMessage = "WARN: ";
+                    break;
+                }
+                case Level.Error:
+                {
+                    logMessage = "ERROR: ";
+                    break;
+                }
+                case Level.Debug:
+                {
+                    logMessage = "DEBUG: ";
+                    break;
+                }
+                default:
+                {
+                    logMessage = "DEBUG: ";
+                    break;
+                 }
+            }
+
+            logMessage = logMessage + Message;
+            logMessage = logMessage + " | " + DateTime.Now.ToString();
+
+
+            Console.WriteLine(logMessage);
         }
     }
 }
--- Module2HW6/Program.cs
+++ Module2HW6/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+using Module2HW6.Logger;
+
+Logger logger = Logger.GetInstance();
+
+logger.Info("Test");
+logger.Warning("Test 2");
+
+Console.ReadLine();
