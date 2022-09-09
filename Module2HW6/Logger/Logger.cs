380a9957e86707566dd9d100271bf022f8530413
Pavel Usachev <pausachev@gmail.com>
2022-09-09 22:40:03 +0300

    created singleton class

---
+++ Module2HW6/Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module2HW6.Logger
+{
+    public sealed class Logger
+    {
+        private Logger() { }
+
+        private static Logger? _instance;
+
+        public static Logger GetInstance()
+        {
+            if (null == _instance)
+            {
+                _instance = new Logger();
+            }
+
+            return _instance;
+        }
+    }
+}
